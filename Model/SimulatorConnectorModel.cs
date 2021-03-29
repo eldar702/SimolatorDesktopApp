7ead44b47b32e73a66bc9df6581e712daeab4ce8
Tzahi <tsahi4332@gmail.com>
2021-03-29 11:52:28 +0300

    Add project files.

---
+++ Model/SimulatorConnectorModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimolatorDesktopApp_1.Model
+{
+    class SimulatorConnectorModel : ISimulatorConnector
+    {
+        // private static readonly Mutex mut = new Mutex();
+        // public int conectionAttempts;
+        TcpClient aClient;
+        public bool isConnected = false;
+
+        public void Connect(string ip, int port)
+        {
+            aClient = new TcpClient(ip, port);
+            isConnected = true;
+            SimulatorModel simulatorModel = new SimulatorModel(this);
+            simulatorModel.startSimulator();
+        }
+
+        public void Disconnect()
+        {
+            if (isConnected)
+            {
+                aClient.Close();
+                isConnected = false;
+            }
+        }
+
+        public string Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(string command)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(command + "\n");
+
+            try
+            {
+                NetworkStream stream = this.aClient.GetStream();
+                stream.Flush();
+                stream.Write(buffer, 0, buffer.Length);
+                // Console.WriteLine("enter write scope");
+            }
+            catch
+            {
+
+            }
+        }
+
+        public string WriteCommand(string command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Model/SimulatorModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimolatorDesktopApp_1.Model
+{
+    class SimulatorModel
+    {
+        private SimulatorConnectorModel _simulatorConnector;
+        private string path = "C:/Users/Asus/source/repos/SimolatorDesktopApp_1_Update_to26.3/SimolatorDesktopApp_1/reg_flight.csv";
+
+        public SimulatorModel(SimulatorConnectorModel simulatorConnector)
+        {
+            _simulatorConnector = simulatorConnector;
+        }
+
+        public void startSimulator()
+        {
+            Thread t = new Thread(delegate()
+            {
+                IuploadFile iuploadFile = new IuploadFile();
+                string[] lines = iuploadFile.ReadFile(path);
+                int numLines = lines.Length;
+                int i = 0;
+                while (i < numLines && _simulatorConnector.isConnected)
+                {
+                    Console.WriteLine(lines[i]);
+                    _simulatorConnector.Write(lines[i]);
+                    Thread.Sleep(100);
+                    i++;
+                }
+                _simulatorConnector.Disconnect();
+            });
+            t.Start();
+        }
+    }
+}
